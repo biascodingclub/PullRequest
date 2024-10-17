bc2e65edfd0b8d135ef8e9e2e6f6e670c1fd2f54
Aleeza-1234 <aleezaop@gmail.com>
2024-10-17 21:26:02 +0530

    Update hello.py
    printing letter by letter

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print("Hello this is my PR!!!")
+from time import sleep
+import random
+from os import system
+
+abc = list(' abcdefghijklmnopqrstuvwxyz ')
+result = 'pull rqst'
+txt = ''
+for i in range(len(result)):
+    for j in range(len(abc)):
+        ch = abc[j]
+        for a in range(10):
+            print(txt + ch)
+        sleep(0.05)
+        system('clear')
+        if ch == result[i]:
+            txt += ch
+            break
+print((result+'\n')*10)
